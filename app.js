0cdadffc6c35da5dc5c25443b757d52eff369f80
yanzacd92 <yanzacd@gmail.com>
2023-03-25 00:20:10 +0000

    added some unit tests

--- app.js
+++ app.js
@@ -1,11 +1,47 @@
+// one euro is:
+let oneEuroIs = {
+    "JPY": 127.9, // japan yen
+    "USD": 1.2, // us dollar
+    "GBP": 0.8, // british pound
+}
+
+// one dollar is:
+let oneDollarIs = {
+    "JPY": 130.75, // japan yen
+    "EUR": 0.93, // euro euros
+    "GBP": 0.82, // british pound
+}
+
+// one yen is:
+let oneYenIs = {
+    "EUR": 0.0071, // euro euro
+    "USD": 0.0076, // us dollar
+    "GBP": 0.0063, // british pound
+}
+
 // this is my function that sums two numbers
 const sum = (a,b) => {
     return a + b
 }
 
+const fromDollarToYen = (usd) => {
+    return usd * oneDollarIs.JPY;
+}
+
+const fromEuroToDollar = (eu) => {
+    return eu * oneEuroIs.USD;
+}
+
+const fromYenToPound = (yen) => {
+    return yen * oneYenIs.GBP;
+}
+
 // just a console log for ourselves.
 console.log(sum(7,3))
+console.log("Dollar to Yen " + fromDollarToYen(5))
+console.log("Euro to Dollar " + fromEuroToDollar(3.5))
+console.log("Yen to Pound " + fromYenToPound(3))
 
 // export the function to be used on other files 
 // (similar to the keyword `export` when using webpack)
-module.exports = { sum };
+module.exports = { sum, fromEuroToDollar, fromYenToPound, fromDollarToYen };
--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 // import the function sum from the app.js file
-const { sum } = require('./app.js');
+const { sum, fromEuroToDollar, fromYenToPound, fromDollarToYen } = require('./app.js');
 
 // start your first test
 test('adds 14 + 9 to equal 23', () => {
@@ -9,3 +9,16 @@ test('adds 14 + 9 to equal 23', () => {
     // we expect the sum of those 2 numbers to be 23
     expect(total).toBe(23);
 });
+
+test("One euro should be 1.206 dollars", function(){
+    // hago mi comparacion (la prueba)
+    expect(fromEuroToDollar(3.5)).toBe(4.2); //1 euro son 1.2 dolares, entonces 3.5 euros deberian ser = (3.5 * 1.2)
+})
+
+test("One yen should be 0.0063 pounds", function(){
+    expect(fromYenToPound(3)).toBe(0.0189);
+})
+
+test("One dollar should be 130.75 yens", function(){
+    expect(fromDollarToYen(5)).toBe(653.75);
+})
